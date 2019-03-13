0be32075260a0757b266246a3d9ddcf0c318ece9
Cameron Morris <cameron.morris@uconn.edu>
2019-03-13 14:18:17 -0400

    config changes for dev server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForecastFormComponent } from './forecast-form/forecast-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastFormComponent
   ],
   imports: [
     BrowserModule,
